688e02e296677cbfca8e6223f59547f7a7dad75c
Laura Polet <laurapplt@gmail.com>
2024-11-30 08:25:53 +0100

    install app.js and server.js Express

---
+++ Backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+
+//CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+
+module.exports = app;
